7f62eff1494adcb595b74fb8fa846fbd9752bd17
fares <djefaflia.fares@gmail.com>
2025-01-15 11:12:49 +0100

    update

--- components/ar/Portfolio/content/Projects.vue
+++ components/ar/Portfolio/content/Projects.vue
@@ -2,8 +2,12 @@
   <div class="mt-4">
     <h1 class="font-bold text-color03 text-3xl">مشاريعي</h1>
     <p class="mr-2 font-light text-color03 text-sm mt-2">جميع أعمالي من الماضي الى الأن</p>
+    <a href="https://www.behance.net/elmoziml">
+      <p class="font-light text-color05 bg-color02 p-3 rounded-lg text-sm mt-2">يمكنك زيارة معرض اعمالي في تصاميم واجهة المستخدم على behance</p>
+    </a>
     <div class="mt-3 my-0 mx-auto sm:flex sm:flex-wrap sm:justify-between lg:max-w-2xl">
-      <a v-for="item in data[0].body" :key="item" :href="item.link" target="_blank" class="inline-block w-full px-3 py-2 rounded-sm transition-all lg:w-80 hover:bg-color02" aria-label="link">
+      <a v-for="item in data[0].body" :key="item" :href="item.link" target="_blank"
+        class="inline-block w-full px-3 py-2 rounded-sm transition-all lg:w-80 hover:bg-color02" aria-label="link">
         <article class="flex items-center">
           <div class="flex justify-center items-center w-11">
             <img :src="item.img">
