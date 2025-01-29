a377b09bb5d785337205f98f1323b14fcdcab119
fares <djefaflia.fares@gmail.com>
2025-01-29 20:29:10 +0100

    Update PORTFOLIO #23

--- components/ar/Portfolio/content/Projects.vue
+++ components/ar/Portfolio/content/Projects.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="mt-4">
     <h1 class="font-bold text-color03 text-3xl">مشاريعي</h1>
-    <p class="mr-2 font-light text-color03 text-sm mt-2">جميع أعمالي من الماضي الى الأن</p>
+    <p class="mr-2 font-light text-color03 text-sm mt-2">جميع مشاريعي في التصميم او البرمجة</p>
     <a href="https://www.behance.net/elmoziml">
-      <p class="font-light text-color05 bg-color02 p-3 rounded-lg text-sm mt-2">يمكنك زيارة معرض اعمالي في تصاميم واجهة المستخدم على behance</p>
+      <p class="font-light text-color05 bg-color02 p-3 rounded-lg text-sm mt-2 flex items-center">
+        <i class="fi fi-brands-behance ml-2 text-[16px] w-[16px] h-[16px]"></i>
+        يمكنك زيارة معرض اعمالي في تصاميم واجهة المستخدم على behance
+      </p>
     </a>
     <div class="mt-3 my-0 mx-auto sm:flex sm:flex-wrap sm:justify-between lg:max-w-2xl">
       <a v-for="item in data[0].body" :key="item" :href="item.link" target="_blank"
--- components/en/Portfolio/content/Projects.vue
+++ components/en/Portfolio/content/Projects.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="mt-4">
     <h1 class="font-bold text-color03 text-3xl">Projects</h1>
-    <p class="ml-2 font-light text-color03 text-sm mt-2">All my works from the past to now</p>
+    <p class="ml-2 font-light text-color03 text-sm mt-2">All my projects in design or programming</p>
+    <a href="https://www.behance.net/elmoziml">
+      <p class="font-light text-color05 bg-color02 p-3 rounded-lg text-sm mt-2 flex items-center">
+        <i class="fi fi-brands-behance mr-2 text-[16px] w-[16px] h-[16px]"></i>
+        You can visit my portfolio for UI designs on Behance
+      </p>
+    </a>
     <div class="mt-3 my-0 mx-auto sm:flex sm:flex-wrap sm:justify-between lg:max-w-2xl">
       <a v-for="item in data[0].body" :key="item" :href="item.link" target="_blank" class="inline-block w-full px-3 py-2 rounded-sm transition-all lg:w-80 hover:bg-color02" aria-label="link">
         <article class="flex items-center">
